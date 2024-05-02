a55cb54921529968568b98d2866077be29b37e9b
Alexey Kudryashov <v-alexeyk@zillowgroup.com>
2024-05-02 23:07:12 +0300

    new expo scrapping

--- arabic_scrapper.rb
+++ arabic_scrapper.rb
@@ -88,8 +88,7 @@ class ScrapingOrganizer
 
       x = 1
 
-      old_elements = []
-
+      data = []
       while true do
         context.driver.execute_script("window.scrollTo(0, #{screen_height*x});")
         x += 1
@@ -97,104 +96,39 @@ class ScrapingOrganizer
 
         scroll_height = context.driver.execute_script("return document.body.scrollHeight;")
 
-        break if (screen_height) * x > scroll_height
-        names_elements = context.wait.until do
-          context.driver.find_elements(xpath: "//*[@id='__next']/div[3]/div/main/div/div[2]/div[2]/div/div/div")
-        end
+        if (screen_height) * x > scroll_height
+          names_elements = context.wait.until do
+            context.driver.find_elements(xpath: '//*[@id="__next"]/div[3]/div/main/div/div[2]/div/div/div/div')
+          end
 
-        puts "names_elements"
-        # debugger
-        puts names_elements.first.find_elements(:css, "a").count
+          links = names_elements.first.find_elements(:css, "a")
 
+          puts "link elements #{links.count}"
 
-        # byebug
-
-        diff = names_elements.first.find_elements(:css, "a") - (old_elements)
-
-        unless diff.empty?
-          array = []
-          diff.each do |info_element|
+          links.each do |link|
             info_hash = {}
 
-            info_element.find_elements(:css, "a").each do |element|
-              info_hash["link"] = element.attribute("href") || "no link"
-            end
+            info_hash["link"] = link.attribute("href") || "no link"
+            info_hash["full_name"] = link.find_elements(:xpath => "*").first.find_elements(:xpath => "*").first.find_elements(:css => "span.sc-a13c392f-0")[0]&.text || "no info"
+            info_hash["position"] = link.find_elements(:xpath => "*").first.find_elements(:xpath => "*").first.find_elements(:css => "span.sc-a13c392f-0")[1]&.text || "no info"
+            info_hash["firm"] = link.find_elements(:xpath => "*").first.find_elements(:xpath => "*").first.find_elements(:css => "span.sc-a13c392f-0")[2]&.text || "no info"
 
-            info_element.find_elements(:css, "span").each do |span|
-              if span.attribute("class").match("sc-1fbfbd68-9")
-                info_hash["full_name"] = span.text || "no info"
-              elsif span.attribute("class").match("sc-1fbfbd68-11")
-                info_hash["position"] = span.text || "no info"
-              elsif span.attribute("class").match("sc-1fbfbd68-12")
-                info_hash["organization"] = span.text || "no info"
-              end
-            end
+            data << info_hash unless info_hash.empty?
+          end
 
-            array << info_hash unless info_hash.empty?
-          end
-          # debugger
-          if old_elements.empty?
-            CSV.open("aaa.csv", "ab", write_headers: true, headers: %w[Link, FullName, Position, Organization]) do |csv|
-              array.each do |row|
-                csv << row.values
-              end
-            end
-          else
-            CSV.open("aaa.csv", "ab") do |csv|
-              array.each do |row|
-                csv << row.values
-              end
-            end
-          end
+          break
         end
+      end
 
-        old_elements = old_elements + diff
-
-        puts "diff.count"
-        puts diff.count
-
-        puts "old_elements.count"
-        puts old_elements.count
+      CSV.open("aaa.csv", "ab", write_headers: true, headers: %w[Link, FullName, Position, Organization]) do |csv|
+        data.each do |row|
+          csv << row.values
+        end
       end
     end
   end
 
-  class ScrapingOrganizer::GetTitles
-    include Interactor
-
-    def call
-      # Executes block while the next button is visible
-      # If it's not it means that we are already on the last page
-      while nxt_button_visible?(context.wait, context.driver) do
-        print_titles(context.driver, context.wait) # Prints titles from current page
-        nxt_button(context.driver).click # Goes to next page
-      end
-    end
-
-    private
-
-    # Finds the next button
-    def nxt_button(driver)
-      driver.find_element(:css, 'li.andes-pagination__button.andes-pagination__button--next')
-    end
-
-    # displayed? tells us if the element is present in the DOM
-    def nxt_button_visible?(wait, driver)
-      wait.until { nxt_button(driver).displayed? }
-    end
-
-    def print_titles(driver, wait)
-      # Finds the titles displayed in the current page
-      titles = wait.until do
-        driver.find_elements(:css, '.main-title')
-      end
-
-      # Prints the element's text
-      titles.map { |title| puts title.text }
-    end
-  end
-
-  organize AcceptCookies, Login#, GetTitles
+  organize AcceptCookies, Login
 end
 
 # Run program
@@ -203,27 +137,26 @@ end
 
 class CSVProcessor
   def self.call
-    file_data = CSV.read("file_data.csv")
+    file_data = CSV.read("aaa.csv")
 
     founders_data = []
     ceo_data = []
 
     file_data.each { |x| founders_data << x if x[2]&.match(/founder/i) }
-    file_data.each { |x| ceo_data << x if x[2]&.match(/ceo/i) }
+    file_data.each { |x| ceo_data << x if x[2]&.match(/ceo/i) && !founders_data.include?(x) }
 
     CSV.open("founders.csv", "ab", write_headers: true, headers: %w[Link, FullName, Position, Organization]) do |csv|
       founders_data.uniq.each do |row|
-        csv << row.values
+        csv << row
       end
     end
 
     CSV.open("ceo.csv", "ab", write_headers: true, headers: %w[Link, FullName, Position, Organization]) do |csv|
       ceo_data.uniq.each do |row|
-        csv << row.values
+
+        csv << row
       end
     end
-
-    debugger
   end
 end
 
